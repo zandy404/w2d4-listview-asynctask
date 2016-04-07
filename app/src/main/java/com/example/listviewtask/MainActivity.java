ec0231b0080e99276e8285d56529ee983dd496e6
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-07 17:38:44 -0400

    first commit

---
+++ app/src/main/java/com/example/listviewtask/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.listviewtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> mArrayList;
+    private ArrayAdapter<String> mAdapter;
+
+    private ListView mListView;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(View view) {
+
+    }
+}
